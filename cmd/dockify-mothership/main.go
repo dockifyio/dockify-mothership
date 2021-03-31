7ac4b16b28e876cb48e697ab9c6eb7c3569f71d1
Ali Talpur <hskalee123@gmail.com>
2021-03-30 21:53:37 -0700

    Add banner to startup

--- cmd/dockify-mothership/main.go
+++ cmd/dockify-mothership/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"github.com/common-nighthawk/go-figure"
 	"github.com/dockifyio/dockify-mothership/api"
 )
 
 func main() {
 	// y := mux.NewRouter()
 	//x := api.App()
+	myFigure := figure.NewFigure("Dockify Mothership", "", true)
+	myFigure.Print()
 	var app api.App
 	app.Initialize()
 	app.Run()
